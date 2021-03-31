48d673e03a651eb56938cb2932fff6b6c4f3b06a
Ｗilson <lowilson180@gmail.com>
2021-03-31 16:22:28 +0800

    Initial Commit

---
+++ BZB_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BZB_Project
+//
+//  Created by GoMax on 2021/3/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
