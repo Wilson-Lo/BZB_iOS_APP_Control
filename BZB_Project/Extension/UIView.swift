a4356f51dd34105225174d936dcc9ea65e2e5b6d
Ｗilson <lowilson180@gmail.com>
2021-04-01 20:02:42 +0800

    1.In the dark mode, will be light.

---
+++ BZB_Project/Extension/UIView.swift
@@ -0,0 +1,54 @@
+//
+//  UIView.swift
+//  BZB_Project
+//
+//  Created by GoMax on 2021/4/1.
+//
+
+import UIKit
+extension UIView{
+    
+    func showToast(text: String, font_size: CGFloat){
+        
+        self.hideToast()
+        let toastLb = UILabel()
+        toastLb.numberOfLines = 0
+        toastLb.lineBreakMode = .byWordWrapping
+        toastLb.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        toastLb.textColor = UIColor.white
+        toastLb.layer.cornerRadius = 10.0
+        toastLb.textAlignment = .center
+        toastLb.font = UIFont.systemFont(ofSize: font_size)
+        toastLb.text = text
+        toastLb.layer.masksToBounds = true
+        toastLb.tag = 9999//tag：hideToast實用來判斷要remove哪個label
+        
+        let maxSize = CGSize(width: self.bounds.width - 40, height: self.bounds.height)
+        var expectedSize = toastLb.sizeThatFits(maxSize)
+        var lbWidth = maxSize.width
+        var lbHeight = maxSize.height
+        if maxSize.width >= expectedSize.width{
+            lbWidth = expectedSize.width
+        }
+        if maxSize.height >= expectedSize.height{
+            lbHeight = expectedSize.height
+        }
+        expectedSize = CGSize(width: lbWidth, height: lbHeight)
+        toastLb.frame = CGRect(x: ((self.bounds.size.width)/2) - ((expectedSize.width + 20)/2), y: self.bounds.height - expectedSize.height - 40 - 20, width: expectedSize.width + 20, height: expectedSize.height + 20)
+        self.addSubview(toastLb)
+        
+        UIView.animate(withDuration: 1.5, delay: 1.5, animations: {
+            toastLb.alpha = 0.0
+        }) { (complete) in
+            toastLb.removeFromSuperview()
+        }
+    }
+    
+    func hideToast(){
+        for view in self.subviews{
+            if view is UILabel , view.tag == 9999{
+                view.removeFromSuperview()
+            }
+        }
+    }
+}
