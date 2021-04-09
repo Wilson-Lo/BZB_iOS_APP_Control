d8e81938d3c7f755cd4b0248c0c59b464ccdeee2
Ｗilson <lowilson180@gmail.com>
2021-04-09 17:02:31 +0800

    1.Remove unused file.

--- BZB_Project/Edid.c
+++
@@ -1,8 +0,0 @@
-//
-//  Edid.c
-//  BZB_Project
-//
-//  Created by GoMax on 2021/4/9.
-//
-
-#include "Edid.h"
--- BZB_Project/zzz.c
+++
@@ -1,8 +0,0 @@
-//
-//  zzz.c
-//  BZB_Project
-//
-//  Created by GoMax on 2021/4/9.
-//
-
-#include "zzz.h"
